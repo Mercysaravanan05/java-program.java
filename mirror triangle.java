2b76fd5314d0f147bb570c2b127aa9c962d11b5e
Mercy Saravanan <mercysaravanan05@gmail.com>
2026-07-29 12:05:29 +0530

    Create mirror triangle.java

---
+++ mirror triangle.java
@@ -0,0 +1,22 @@
+public class pat {
+    static void fi(int n){
+        for(int i=1;i<=n;i++){
+            
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+public static void main(String[]args){
+        int arr[]={3,5,8};
+        for(int i=0;i<3;i++){
+            System.out.println(arr[i]);
+             fi(arr[i]);
+        }
+     }
+                
+}
