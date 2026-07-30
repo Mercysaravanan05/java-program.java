6a4844e358f2fb65f6895498846b55c25c20f5f0
Mercy Saravanan <mercysaravanan05@gmail.com>
2026-07-30 23:17:34 +0530

    Create lucky digit.java

---
+++ lucky digit.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner s=new Scanner(System.in);
+	    String a=s.nextLine();
+	    
+	    
+	            System.out.println(a.charAt(6));
+	       
+	}
+}
